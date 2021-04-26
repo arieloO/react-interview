cd7034ad260abf62012a3723bac34374c6f8d250
Ariel <ariel.levyk@gmail.com>
2021-04-26 17:07:23 +0200

    splited state and data request, transfered state & functions down to MovieList

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,27 +1,27 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { movies$ } from "./data/movies.js";
-import MovieGrid from "./components/MovieGrid";
+import MovieList from "./components/MovieList";
 
 function App() {
-    const [movies, setMovies] = useState([]);
+    const [moviesRequest, setMoviesRequest] = useState(null);
 
     useEffect(() => {
-        movies$.then((data) => setMovies(data));
-    }, []);
+        movies$.then((data) => setMoviesRequest(data));
+    }, [moviesRequest]);
 
-    const deleteMovie = (id) => {
-        console.log("delete this movie : ", id);
-        let newState = movies.filter((movie) => movie.id !== id);
-        setMovies(newState);
-    };
+    // const deleteMovie = (id) => {
+    //     console.log("delete this movie : ", id);
+    //     let newState = movies.filter((movie) => movie.id !== id);
+    //     setMovies(newState);
+    // };
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Movies !</h1>
             </header>
-            <MovieGrid movies={movies} deleteMovie={deleteMovie} />
+            {moviesRequest ? <MovieList movies={moviesRequest} /> : null}
         </div>
     );
 }
