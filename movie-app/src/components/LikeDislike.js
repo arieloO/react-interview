b149aaafec868ad7c3340f76c396cf2444607a64
Ariel <ariel.levyk@gmail.com>
2021-04-26 14:58:05 +0200

    changed like/dislike style and layout

--- movie-app/src/components/LikeDislike.js
+++ movie-app/src/components/LikeDislike.js
@@ -1,9 +1,9 @@
 const LikeDislike = ({ likes, dislikes, id }) => {
     return (
         <div className="like-dislike">
+            <meter min="0" max={likes + dislikes} value={likes}></meter>
             <div>likes : {likes}</div>
             <div>dislikes : {dislikes}</div>
-            <meter min="0" max={likes + dislikes} value={likes}></meter>
         </div>
     );
 };
